2adbe2cb1f3dffe9d93580ff6bfc81a61d271ad2
Ariel Luke <55467199+ariel-web@users.noreply.github.com>
2022-06-04 11:15:23 -0500

    personalización de la seccion home

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
         }else{
             $('.navbar').removeClass("sticky");
         }
-        
+         
         // scroll-up button show/hide script
         if(this.scrollY > 500){
             $('.scroll-up-btn').addClass("show");
