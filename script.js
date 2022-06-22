0b21e7741a26497ad87a0c35942ed6bd5cd884f0
Ariel Luke <55467199+ariel-web@users.noreply.github.com>
2022-06-21 21:22:51 -0500

    Galeria de Proyectos

--- script.js
+++ script.js
@@ -98,3 +98,52 @@ menuLinks.forEach(menuLink => {
     }
     
 })
+
+
+const filterItem = document.querySelector(".items");
+const filterImg = document.querySelectorAll(".galeria .container-galeria");
+window.onload = ()=>{ 
+  filterItem.onclick = (selectedItem)=>{ 
+    if(selectedItem.target.classList.contains("item")){
+      filterItem.querySelector(".active").classList.remove("active"); 
+      selectedItem.target.classList.add("active"); 
+
+      let filterName = selectedItem.target.getAttribute("data-name");
+      filterImg.forEach((image) => {
+        let filterImges = image.getAttribute("data-name"); 
+        if((filterImges == filterName) || (filterName == "all")){
+          image.classList.remove("hide"); 
+          image.classList.add("show"); 
+        }else{
+          image.classList.add("hide"); 
+          image.classList.remove("show"); 
+        }
+      });
+    }
+  }
+  for (let i = 0; i < filterImg.length; i++) {
+    filterImg[i].setAttribute("onclick", "preview(this)");
+  }
+}
+
+
+
+const previewBox = document.querySelector(".preview-box"),
+previewImg = previewBox.querySelector("img"),
+closeIcon = previewBox.querySelector(".icon"),
+shadow = document.querySelector(".shadow");
+
+function preview(element){
+  //once user click on any image then remove the scroll bar of the body, so user cant scroll up or down
+  document.querySelector("body").style.overflow = "hidden";
+  let selectedPrevImg = element.querySelector("img").src; //getting user clicked image source link and stored in a variable
+  let selectedImgCategory = element.getAttribute("data-name"); //getting user clicked image data-name value
+  previewImg.src = selectedPrevImg; //passing the user clicked image source in preview image source //passing user clicked data-name value in category name
+  previewBox.classList.add("show"); //show the preview image box
+  shadow.classList.add("show"); //show the light grey background
+  closeIcon.onclick = ()=>{ //if user click on close icon of preview box
+    previewBox.classList.remove("show"); //hide the preview box
+    shadow.classList.remove("show"); //hide the light grey background
+    document.querySelector("body").style.overflow = "auto"; //show the scroll bar on body
+  }
+}
